caf355fcf4bcc6555143cb44653236ca04faabbd
danae <cdanae.rod@gmail.com>
2024-03-05 13:22:08 -0600

    string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Danae';
+  age = 29;
+  image = 'https://source.unsplash.com/random';
 }
